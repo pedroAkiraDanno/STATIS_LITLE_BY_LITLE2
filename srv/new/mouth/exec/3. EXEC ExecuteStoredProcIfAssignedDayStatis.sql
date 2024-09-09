7b49f2087f0a1bf7f8306f00b732bbf7d4acdc81
PedroAkiaDanno <pedro-akira@hotimail>
2024-09-09 12:40:48 -0300

    add comment about job

--- srv/new/mouth/exec/3. EXEC ExecuteStoredProcIfAssignedDayStatis.sql
+++ srv/new/mouth/exec/3. EXEC ExecuteStoredProcIfAssignedDayStatis.sql
@@ -83,7 +83,7 @@
 
 
 
-/*
+
 
 --JOB 
 
@@ -94,75 +94,73 @@
 
 
 
-USE [msdb]
-GO
+--USE [msdb]
+--GO
 
-/****** Object:  Job [MaintenanceStatis]    Script Date: 9/9/2024 12:31:01 PM ******/
-BEGIN TRANSACTION
-DECLARE @ReturnCode INT
-SELECT @ReturnCode = 0
-/****** Object:  JobCategory [Database Maintenance]    Script Date: 9/9/2024 12:31:01 PM ******/
-IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Database Maintenance' AND category_class=1)
-BEGIN
-EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Database Maintenance'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+--/****** Object:  Job [MaintenanceStatis]    Script Date: 9/9/2024 12:31:01 PM ******/
+--BEGIN TRANSACTION
+--DECLARE @ReturnCode INT
+--SELECT @ReturnCode = 0
+--/****** Object:  JobCategory [Database Maintenance]    Script Date: 9/9/2024 12:31:01 PM ******/
+--IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Database Maintenance' AND category_class=1)
+--BEGIN
+--EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Database Maintenance'
+--IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 
-END
+--END
 
-DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'MaintenanceStatis', 
-		@enabled=0, 
-		@notify_level_eventlog=0, 
-		@notify_level_email=0, 
-		@notify_level_netsend=0, 
-		@notify_level_page=0, 
-		@delete_level=0, 
-		@description=N'No description available.', 
-		@category_name=N'Database Maintenance', 
-		@owner_login_name=N'supersa', @job_id = @jobId OUTPUT			-- change the owner 
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Step1]    Script Date: 9/9/2024 12:31:01 PM ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Step1', 
-		@step_id=1, 
-		@cmdexec_success_code=0, 
-		@on_success_action=1, 
-		@on_success_step_id=0, 
-		@on_fail_action=2, 
-		@on_fail_step_id=0, 
-		@retry_attempts=0, 
-		@retry_interval=0, 
-		@os_run_priority=0, @subsystem=N'TSQL', 
-		@command=N'EXEC ExecuteStoredProcIfAssignedDayStatis;
-GO ', 
-		@database_name=N'CloudADM', 
-		@flags=0
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Scheduler1', 
-		@enabled=1, 
-		@freq_type=4, 
-		@freq_interval=1, 
-		@freq_subday_type=1, 
-		@freq_subday_interval=0, 
-		@freq_relative_interval=0, 
-		@freq_recurrence_factor=0, 
-		@active_start_date=20240909, 
-		@active_end_date=99991231, 
-		@active_start_time=0, 
-		@active_end_time=235959, 
-		@schedule_uid=N'e2f87ebf-03e9-4927-a419-b3867822d8f7'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-COMMIT TRANSACTION
-GOTO EndSave
-QuitWithRollback:
-    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
-EndSave:
-GO
-
-
+--DECLARE @jobId BINARY(16)
+--EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'MaintenanceStatis', 
+--		@enabled=0, 
+--		@notify_level_eventlog=0, 
+--		@notify_level_email=0, 
+--		@notify_level_netsend=0, 
+--		@notify_level_page=0, 
+--		@delete_level=0, 
+--		@description=N'No description available.', 
+--		@category_name=N'Database Maintenance', 
+--		@owner_login_name=N'supersa', @job_id = @jobId OUTPUT			-- change the owner 
+--IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+--/****** Object:  Step [Step1]    Script Date: 9/9/2024 12:31:01 PM ******/
+--EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Step1', 
+--		@step_id=1, 
+--		@cmdexec_success_code=0, 
+--		@on_success_action=1, 
+--		@on_success_step_id=0, 
+--		@on_fail_action=2, 
+--		@on_fail_step_id=0, 
+--		@retry_attempts=0, 
+--		@retry_interval=0, 
+--		@os_run_priority=0, @subsystem=N'TSQL', 
+--		@command=N'EXEC ExecuteStoredProcIfAssignedDayStatis;
+--GO ', 
+--		@database_name=N'CloudADM', 
+--		@flags=0
+--IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+--EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+--IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+--EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Scheduler1', 
+--		@enabled=1, 
+--		@freq_type=4, 
+--		@freq_interval=1, 
+--		@freq_subday_type=1, 
+--		@freq_subday_interval=0, 
+--		@freq_relative_interval=0, 
+--		@freq_recurrence_factor=0, 
+--		@active_start_date=20240909, 
+--		@active_end_date=99991231, 
+--		@active_start_time=0, 
+--		@active_end_time=235959, 
+--		@schedule_uid=N'e2f87ebf-03e9-4927-a419-b3867822d8f7'
+--IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+--EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+--IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+--COMMIT TRANSACTION
+--GOTO EndSave
+--QuitWithRollback:
+--    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+--EndSave:
+--GO
 
 
 
@@ -170,7 +168,10 @@ GO
 
 
 
-*/
+
+
+
+
 
 
 
